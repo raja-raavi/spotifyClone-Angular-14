db98b4ed5b98dd396d566e12e8826ed220867e55
raja-raavi <rajaraavi999@gmail.com>
2023-12-17 23:20:32 +0530

    created components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SongsCardComponent } from './components/songs-card/songs-card.component';
+import { TopNavComponent } from './components/top-nav/top-nav.component';
+import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { PlaylistComponent } from './pages/playlist/playlist.component';
+import { SearchComponent } from './pages/search/search.component';
+import { SignupComponent } from './pages/signup/signup.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SongsCardComponent,
+    TopNavComponent,
+    HomeComponent,
+    LoginComponent,
+    PlaylistComponent,
+    SearchComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/songs-card/songs-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-songs-card',
+  templateUrl: './songs-card.component.html',
+  styleUrls: ['./songs-card.component.css']
+})
+export class SongsCardComponent {
+
+}
